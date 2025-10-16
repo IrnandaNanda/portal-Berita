923841e2baa45d89ada849b428d8ace654ef1000
IrnandaNanda <irnandatri1509@gmail.com>
2025-10-16 23:50:14 +0700

    Mobile

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,5 +3,5 @@
 @php
     $classes = $current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white';
 @endphp
-
+                                       {{-- ini props --}}
 <a href="{{ $href }}" {{ $attributes->merge(['class' => 'rounded-md px-3 py-2 text-sm font-medium ' . $classes]) }}> {{ $slot }} </a>
