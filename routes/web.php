d1a61f6f9324f94329c6ec0572d7e5b01831993c
IrnandaNanda <irnandatri1509@gmail.com>
2025-10-18 22:34:07 +0700

    Halaman Single Blog Post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,22 +10,54 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     $posts = [
         [
+            'id' => 1,
+            'slug' => 'Judul-Artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Irnanda nanda',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ipsum, corporis harum esse sapiente velit
             et atque natus magnam aspernatur, quo dolor? Veniam totam repellat modi minus rem voluptatibus nostrum.'
         ],
         [
+            'id' => 2,
+            'slug' => 'Judul-Artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Roma Arden',
             'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut enim consectetur sit neque ea, excepturi sapiente. Excepturi, velit laborum, et facere iure hic corporis fugiat voluptas, deserunt corrupti neque magni.'
         ]
     ];
-    return view('posts', ['title' => 'Blog Page', 'posts' => $posts]);
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'slug' => 'Judul-Artikel-1',
+            'author' => 'Irnanda nanda',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ipsum, corporis harum esse sapiente velit
+            et atque natus magnam aspernatur, quo dolor? Veniam totam repellat modi minus rem voluptatibus nostrum.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'slug' => 'Judul-Artikel-2',
+            'author' => 'Roma Arden',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut enim consectetur sit neque ea, excepturi sapiente. Excepturi, velit laborum, et facere iure hic corporis fugiat voluptas, deserunt corrupti neque magni.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    if (!$post) abort(404);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/about', function () {
-    return view('about', ['title' => 'About Me']);
+    return view('about', ['title' => 'About']);
 });
 
 Route::get('/contact', function () {
