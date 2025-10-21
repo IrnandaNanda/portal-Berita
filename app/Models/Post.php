22e69c689267b9fe1c43f2603f76b3f6a987a849
IrnandaNanda <irnandatri1509@gmail.com>
2025-10-21 22:59:07 +0700

    Tinker & Mass assigments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,4 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model {}
+class Post extends Model {
+    // Untuk Melindungi dari user yang ingin mengirimkan data melalui Request HTTP
+    protected   $fillable = ['title', 'author', 'slug', 'body'];
+
+}
+
