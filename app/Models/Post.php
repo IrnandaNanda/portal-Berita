77cfb4ae0bd3025093c4b769aa9ff2bb093d3c93
IrnandaNanda <irnandatri1509@gmail.com>
2025-10-27 03:04:03 +0700

    Model Factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model {
+class Post extends Model
+{
+    // untuk membuat data dummy
+    Use HasFactory;
+
     // Untuk Melindungi dari user yang ingin mengirimkan data melalui Request HTTP
     protected   $fillable = ['title', 'author', 'slug', 'body'];
-
 }
-
