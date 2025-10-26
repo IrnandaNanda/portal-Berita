11c769f0ffd43a1cf6c32786961e3043d89e5a64
IrnandaNanda <irnandatri1509@gmail.com>
2025-10-27 05:36:20 +0700

    Eloquent Relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +45,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    //Untuk relationship database one to many
+    public function posts(): HasMany
+    {
+        //Hubungkan ke model Post dengan nama table author_id
+        return $this->hasMany(Post::class, 'author_id');
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="#">{{ $post['author'] }}</a> | 25 Januari 2025
+                <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | 25 Januari 2025
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}
             </p>
